b46e7f56d273d7a16a8b5d3504e7c895199c3b45
k-avtonomov <kirill.avtonomov@yandex.ru>
2021-04-05 09:24:51 +0300

    правки

--- src/pages/address/AssignDivisions.js
+++ src/pages/address/AssignDivisions.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useMemo, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Route, Switch, useRouteMatch } from "react-router-dom";
-import { getDivisionByLogin } from "utils/helpers";
 import { useTableComponent } from "components/table/useTableComponent";
 import {
   NumberFilter,
@@ -9,7 +8,7 @@ import {
   StringFilter,
 } from "components/table/Filters";
 import { NumberCell, StringCell } from "components/table/Cell";
-import { getItem, getUserId } from "utils/user";
+import { getUserId } from "utils/user";
 import { Button } from "@material-ui/core";
 import {
   assignDivisionToHouse,
