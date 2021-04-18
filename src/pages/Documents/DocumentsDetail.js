85ef2ca9e1cde0c18852281ff6a5bfc7586c669f
k-avtonomov <kirill.avtonomov@yandex.ru>
2021-04-18 11:31:07 +0300

    Родственники

--- src/pages/Documents/DocumentsDetail.js
+++ src/pages/Documents/DocumentsDetail.js
@@ -14,6 +14,7 @@ import {
   DialogContent,
   DialogTitle,
   Paper,
+  Typography,
 } from "@material-ui/core";
 import { runRpc, runRpcRecords, runRpcSingleRecord } from "utils/rpc";
 import { makeStyles } from "@material-ui/core/styles";
@@ -39,8 +40,8 @@ export const DocumentsDetail = ({
   setOpen,
   onSubmit = () => {},
 }) => {
-  const [roles, setRoles] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [jbchild, setjbchild] = useState([]);
 
   const classes = useStyles();
   const {
@@ -71,7 +72,7 @@ export const DocumentsDetail = ({
       c_notice: "",
       f_user: "",
       sn_delete: false,
-      jb_child: [],
+      // jb_child: [],
       c_import_doc: "",
       c_import_warning: "",
       ...record,
@@ -157,6 +158,13 @@ export const DocumentsDetail = ({
     },
   });
 
+  const onChangeJbChild = (id, name) => {
+    return (e) => {
+      debugger;
+      jbchild[id] = { ...jbchild[id], [name]: e.target.value };
+      setjbchild(jbchild);
+    };
+  };
   const loadData = async () => {
     if (record && record.id) {
       setLoading(true);
@@ -175,36 +183,16 @@ export const DocumentsDetail = ({
           },
         ],
       });
+      debugger;
+      setjbchild(dd_document.jb_child || []);
       setValues(dd_document);
       setLoading(false);
     }
-    // const _claims = await runRpcRecords({
-    //   action: "pd_userinroles",
-    //   method: "Query",
-    //   data: [
-    //     {
-    //       limit: 1000,
-    //       select: "id,f_role,f_user,f_user___c_login,f_user___c_first_name",
-    //       filter: [
-    //         {
-    //           property: "f_user",
-    //           value: record.id,
-    //           operator: "=",
-    //         },
-    //       ],
-    //     },
-    //   ],
-    // });
-    // if (_claims.length) {
-    //   setFieldValue("c_claims", _claims[0].f_role);
-    //   setFieldValue("f_role", _claims[0].id);
-    // }
   };
 
   useEffect(() => {
     if (record && record.id) {
       loadData();
-      console.log("values", values);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [record]);
@@ -234,70 +222,76 @@ export const DocumentsDetail = ({
         >
           <Box display="flex" flexDirection={"column"}>
             <Box
-              display="grid"
-              gridGap="15px"
-              gridTemplateColumns="1fr 1fr"
-              margin="15px 0 0 0"
+              display="flex"
+              flexDirection={"column"}
               border="1px solid #c4c4c4"
-              padding="10px"
               borderRadius="5px"
             >
-              <TextField
-                {...options}
-                label={"Номер"}
-                name={"n_number"}
-                value={values.n_number}
-              />
-              <TextField
-                {...options}
-                label={"ФИО заявителя"}
-                name={"c_fio"}
-                value={values.c_fio}
-              />
-              <KeyboardDatePicker
-                autoOk
-                variant="inline"
-                inputVariant="outlined"
-                name={"d_birthday"}
-                label={"Дата рождения"}
-                format="DD.MM.YYYY"
-                size="small"
-                InputAdornmentProps={{ position: "end" }}
-                value={values.d_birthday}
-                onChange={(date) => handleChange(date.format("DD.MM.YYYY"))}
-              />
-              <TextField
-                {...options}
-                label={"Возраст на момент постановки"}
-                name={"n_year"}
-                value={values.n_year}
-              />
-              <TextField
-                {...options}
-                label={"Реквизиты документа, удостоверяющего личность"}
-                name={"c_document"}
-                value={values.c_document}
-              />
-              <TextField
-                {...options}
-                label={"Адрес, телефон"}
-                name={"c_address"}
-                value={values.c_address}
-              />
-              {/* <TextField
+              <Typography style={{ margin: "15px 0 0 15px" }}>
+                Заявитель
+              </Typography>
+              <Box
+                display="grid"
+                gridGap="15px"
+                gridTemplateColumns="1fr 1fr"
+                padding="10px"
+              >
+                <TextField
+                  {...options}
+                  label={"Номер"}
+                  name={"n_number"}
+                  value={values.n_number}
+                />
+                <TextField
+                  {...options}
+                  label={"ФИО заявителя"}
+                  name={"c_fio"}
+                  value={values.c_fio}
+                />
+                <KeyboardDatePicker
+                  autoOk
+                  variant="inline"
+                  inputVariant="outlined"
+                  name={"d_birthday"}
+                  label={"Дата рождения"}
+                  format="DD.MM.YYYY"
+                  size="small"
+                  InputAdornmentProps={{ position: "end" }}
+                  value={values.d_birthday}
+                  onChange={(date) => handleChange(date.format("DD.MM.YYYY"))}
+                />
+                <TextField
+                  {...options}
+                  label={"Возраст на момент постановки"}
+                  name={"n_year"}
+                  value={values.n_year}
+                />
+                <TextField
+                  {...options}
+                  label={"Реквизиты документа, удостоверяющего личность"}
+                  name={"c_document"}
+                  value={values.c_document}
+                />
+                <TextField
+                  {...options}
+                  label={"Адрес, телефон"}
+                  name={"c_address"}
+                  value={values.c_address}
+                />
+                {/* <TextField
                 {...options}
                 label={"Дата подачи заявления"}
                 name={"d_date"}
                 value={values.d_date}
               /> */}
 
-              {/* <TextField
+                {/* <TextField
               {...options}
               label={"Идентификатор пользователя"}
               name={"f_user"}
               value={values.f_user}
             /> */}
-              {/* <TextField
+                {/* <TextField
               label="sn_delete"
               name={"sn_delete"}
               value={values.sn_delete}
@@ -309,7 +303,7 @@ export const DocumentsDetail = ({
               <MenuItem value={false}>Нет</MenuItem>
             </TextField> */}
 
-              {/* <TextField
+                {/* <TextField
               label="Роли"
               name={"c_claims"}
               value={values.c_claims}
@@ -327,138 +321,246 @@ export const DocumentsDetail = ({
               value={values.c_email}
               {...options}
             /> */}
+              </Box>
             </Box>
+
             <Box
-              display="grid"
-              gridGap="15px"
-              gridTemplateColumns="1fr 1fr"
-              margin="15px 0 0 0"
+              display="flex"
+              flexDirection={"column"}
               border="1px solid #c4c4c4"
-              padding="10px"
               borderRadius="5px"
+              margin="15px 0 0 0"
             >
-              <TextField
-                {...options}
-                label={"Время подачи заявления"}
-                name={"c_time"}
-                value={values.c_time}
-              />
-              <TextField
-                {...options}
-                label={"Цель использования земельного участка"}
-                name={"c_intent"}
-                value={values.c_intent}
-              />
-              <TextField
-                {...options}
-                label={"Постановление о постановке на учет"}
-                name={"c_account"}
-                value={values.c_account}
-              />
-              {/* <TextField
+              <Typography style={{ margin: "15px 0 0 15px" }}>
+                Заявление
+              </Typography>
+              <Box
+                display="grid"
+                gridGap="15px"
+                gridTemplateColumns="1fr 1fr"
+                padding="10px"
+              >
+                <TextField
+                  {...options}
+                  label={"Время подачи заявления"}
+                  name={"c_time"}
+                  value={values.c_time}
+                />
+                <TextField
+                  {...options}
+                  label={"Цель использования земельного участка"}
+                  name={"c_intent"}
+                  value={values.c_intent}
+                />
+                <TextField
+                  {...options}
+                  label={"Постановление о постановке на учет"}
+                  name={"c_account"}
+                  value={values.c_account}
+                />
+                {/* <TextField
                 {...options}
                 label={"Решение о снятии с учета"}
                 name={"d_take_off_solution"}
                 value={values.d_take_off_solution}
               /> */}
-              <KeyboardDatePicker
-                autoOk
-                variant="inline"
-                inputVariant="outlined"
-                label={"Решение о снятии с учета"}
-                name={"d_take_off_solution"}
-                value={values.d_take_off_solution}
-                format="DD.MM.YYYY"
-                size="small"
-                InputAdornmentProps={{ position: "end" }}
-                onChange={(date) => handleChange(date.format("DD.MM.YYYY"))}
-              />
-              {/* <TextField
+                <KeyboardDatePicker
+                  autoOk
+                  variant="inline"
+                  inputVariant="outlined"
+                  label={"Решение о снятии с учета"}
+                  name={"d_take_off_solution"}
+                  value={values.d_take_off_solution}
+                  format="DD.MM.YYYY"
+                  size="small"
+                  InputAdornmentProps={{ position: "end" }}
+                  onChange={(date) => handleChange(date.format("DD.MM.YYYY"))}
+                />
+                {/* <TextField
                 {...options}
                 label={"Сообщение заявителю о снятии с учета"}
                 name={"d_take_off_message"}
                 value={values.d_take_off_message}
               /> */}
-              <KeyboardDatePicker
-                autoOk
-                variant="inline"
-                inputVariant="outlined"
-                label={"Сообщение заявителю о снятии с учета"}
-                name={"d_take_off_message"}
-                value={values.d_take_off_message}
-                format="DD.MM.YYYY"
-                size="small"
-                InputAdornmentProps={{ position: "end" }}
-                onChange={(date) => handleChange(date.format("DD.MM.YYYY"))}
-              />
-              <TextField
-                {...options}
-                label={"Примечание"}
-                multiline
-                rows={4}
-                name={"c_notice"}
-                value={values.c_notice}
-              />
+                <KeyboardDatePicker
+                  autoOk
+                  variant="inline"
+                  inputVariant="outlined"
+                  label={"Сообщение заявителю о снятии с учета"}
+                  name={"d_take_off_message"}
+                  value={values.d_take_off_message}
+                  format="DD.MM.YYYY"
+                  size="small"
+                  InputAdornmentProps={{ position: "end" }}
+                  onChange={(date) => handleChange(date.format("DD.MM.YYYY"))}
+                />
+                <TextField
+                  {...options}
+                  label={"Примечание"}
+                  multiline
+                  rows={4}
+                  name={"c_notice"}
+                  value={values.c_notice}
+                />
+              </Box>
             </Box>
             <Box
-              display="grid"
-              gridGap="15px"
-              gridTemplateColumns="1fr 1fr"
+              display="flex"
+              flexDirection={"column"}
+              border="1px solid #c4c4c4"
+              borderRadius="5px"
               margin="15px 0 0 0"
-              border="1px solid #c4c4c4"
-              padding="10px"
-              borderRadius="5px"
             >
-              <TextField
-                {...options}
-                label={"Дата и номер принятия решения"}
-                name={"c_accept"}
-                value={values.c_accept}
-              />
-              <TextField
-                {...options}
-                label={"Кадастровый номер принятия решения"}
-                name={"c_earth"}
-                value={values.c_earth}
-              />
+              <Typography style={{ margin: "15px 0 0 15px" }}>
+                Решение
+              </Typography>
+              <Box
+                display="grid"
+                gridGap="15px"
+                gridTemplateColumns="1fr 1fr"
+                padding="10px"
+              >
+                <TextField
+                  {...options}
+                  label={"Дата и номер принятия решения"}
+                  name={"c_accept"}
+                  value={values.c_accept}
+                />
+                <TextField
+                  {...options}
+                  label={"Кадастровый номер принятия решения"}
+                  name={"c_earth"}
+                  value={values.c_earth}
+                />
+              </Box>
             </Box>
+
             <Box
-              display="grid"
-              gridGap="15px"
-              gridTemplateColumns="1fr 1fr"
+              display="flex"
+              flexDirection={"column"}
+              border="1px solid #c4c4c4"
+              borderRadius="5px"
               margin="15px 0 0 0"
-              border="1px solid #c4c4c4"
-              padding="10px"
-              borderRadius="5px"
             >
-              <TextField
-                {...options}
-                label={"Дочерние записи"}
-                name={"jb_child"}
-                value={values.c_import_warning}
-              />
+              <Typography style={{ margin: "15px 0 0 15px" }}>
+                Родственники
+              </Typography>
+              {jbchild.map((item, id) => {
+                return (
+                  <>
+                    <Box
+                      display="grid"
+                      gridGap="15px"
+                      gridTemplateColumns="1fr 1fr"
+                      margin="15px 0 0 0"
+                      padding="0 10px 10px 10px"
+                    >
+                      <TextField
+                        {...options}
+                        onChange={onChangeJbChild(id, "c_fio")}
+                        label={"ФИО"}
+                        name={`c_fio`}
+                        value={item.c_fio || ""}
+                      />
+                      {/* <TextField
+                        {...options}
+                        onChange={onChangeJbChild(id, "n_year")}
+                        label={"Возраст"}
+                        name={`n_year`}
+                        value={item.n_year || ""}
+                      /> */}
+                      <TextField
+                        {...options}
+                        onChange={onChangeJbChild(id, "c_address")}
+                        label={"Адрес, телефон"}
+                        name={`c_address`}
+                        value={item.c_address || ""}
+                      />
+                      <TextField
+                        {...options}
+                        onChange={onChangeJbChild(id, "c_document")}
+                        label={"Реквизиты документа, удостоверяющего личность"}
+                        name={`c_document`}
+                        value={item.c_document || ""}
+                      />
+                      <KeyboardDatePicker
+                        autoOk
+                        variant="inline"
+                        inputVariant="outlined"
+                        label={"Дата рождения"}
+                        name={`d_birthday`}
+                        value={item.d_birthday || moment()}
+                        format="DD.MM.YYYY"
+                        size="small"
+                        InputAdornmentProps={{ position: "end" }}
+                        // onChange={(date) =>
+                        //   handleChange(date.format("DD.MM.YYYY"))
+                        // }
+                      />
+                    </Box>
+                    <Button
+                      style={{ width: "100px", margin: "0 10px 20px auto" }}
+                      onClick={() => {
+                        // debugger;
+                        // console.log(values.jb_child);
+                        jbchild.splice(id, 1);
+                        setjbchild([...jbchild]);
+                        // setFieldValue()
+                      }}
+                      color="primary"
+                      variant="contained"
+                    >
+                      Удалить
+                    </Button>
+                  </>
+                );
+              })}
+              {
+                <Button
+                  style={{ margin: "0 15px 15px 15px" }}
+                  onClick={() => {
+                    // debugger;
+                    // console.log(values.jb_child);
+                    setjbchild([...jbchild, {}]);
+                    // setFieldValue()
+                  }}
+                  color="primary"
+                  variant="contained"
+                >
+                  Добавить
+                </Button>
+              }
             </Box>
             <Box
-              display="grid"
-              gridGap="15px"
-              gridTemplateColumns="1fr 1fr"
-              margin="15px 0 15px 0"
+              display="flex"
+              flexDirection={"column"}
               border="1px solid #c4c4c4"
-              padding="10px"
               borderRadius="5px"
+              margin="15px 0 0 0"
             >
-              <TextField
-                {...options}
-                label={"В рамках какого документа был импорт"}
-                name={"c_import_doc"}
-                value={values.c_import_doc}
-              />
-              <TextField
-                {...options}
-                label={"Текст предупреждения"}
-                name={"c_import_warning"}
-                value={values.c_import_warning}
-              />
+              <Typography style={{ margin: "15px 0 0 15px" }}>
+                Технические данные
+              </Typography>
+              <Box
+                display="grid"
+                gridGap="15px"
+                gridTemplateColumns="1fr 1fr"
+                padding="10px"
+                borderRadius="5px"
+              >
+                <TextField
+                  {...options}
+                  label={"В рамках какого документа был импорт"}
+                  name={"c_import_doc"}
+                  value={values.c_import_doc}
+                />
+                <TextField
+                  {...options}
+                  label={"Текст предупреждения"}
+                  name={"c_import_warning"}
+                  value={values.c_import_warning}
+                />
+              </Box>
             </Box>
           </Box>
         </MuiPickersUtilsProvider>
